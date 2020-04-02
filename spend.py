5660bbb50f5cc35110931253e2070fead34850c7
sunnie1239 <sunnie1239@gmail.com>
2020-04-01 17:43:55 -0700

    split each item and print

--- spend.py
+++ spend.py
@@ -1,14 +1,19 @@
 product = []
 
 while True :
-	item = input("今天買了什麼? (離開則輸入q)")
+	item = input("今天買了什麼? (離開則輸入q) ")
 	if item == 'q' :
 		break
-	price = input('請輸入金額:')
+	price = input('請輸入金額: ')
 	# p = []
 	# p.append(item)
 	# p.append(price)
 	# product.append(p)
 	product.append([item, price]) # 二維list
 
-print(product)    
+print('product list 的資料內容: ', product)    
+print('product list 有幾個項目: ', len(product))
+
+# 依序列出資料內容
+for i in range(len(product)) :
+	print(product[i][0], '價格是', product[i][1])
